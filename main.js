02a4079829a7edaa860599bf65172c65c6442d28
Ayman-23 <aymanctg24@gmail.com>
2021-07-27 19:50:20 +0600

    1st commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+main.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+main.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
